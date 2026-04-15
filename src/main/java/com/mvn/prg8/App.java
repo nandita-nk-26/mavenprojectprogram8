18224bc09154007b5ac64bcbaa2f2c55e632b7ba
nandita-nk-26 <nandita-nk-26>
2026-04-15 05:37:49 +0530

    first commit

---
+++ src/main/java/com/mvn/prg8/App.java
@@ -0,0 +1,13 @@
+package com.mvn.prg8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
